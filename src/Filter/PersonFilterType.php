24ad898412b631b94b15987b30a3a6b1e067f84c
Daniel Burckhardt <daniel.burckhardt@sur-gmbh.ch>
2025-03-11 10:03:49 +0100

    Add fulltext search across multiple fields

--- src/Filter/PersonFilterType.php
+++ src/Filter/PersonFilterType.php
@@ -3,6 +3,7 @@
 // PersonFilterType.php
 namespace App\Filter;
 
+use phpDocumentor\Reflection\DocBlock\Tags\Var_;
 use Symfony\Component\Form\FormBuilderInterface;
 use Spiriit\Bundle\FormFilterBundle\Filter\Query\QueryInterface;
 use Spiriit\Bundle\FormFilterBundle\Filter\Form\Type as Filters;
@@ -11,6 +12,7 @@ class PersonFilterType extends BaseFilterType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        /*
         $builder->add('nameFull', Filters\TextFilterType::class, [
             'label' => 'Name',
             'apply_filter' => static function (QueryInterface $filterQuery, $field, $values) {
@@ -26,6 +28,48 @@ class PersonFilterType extends BaseFilterType
 
                 // expression parameters
                 $parameters = [$paramName => self::mysql_regex_escape($values['value'])];
+
+                return $filterQuery->createCondition($expression, $parameters);
+            }
+        ]);
+        */
+
+        $builder->add('fulltext', Filters\TextFilterType::class, [
+            'label' => 'Suche',
+            'apply_filter' => static function (QueryInterface $filterQuery, $field, $values) {
+                if (empty($values['value'])) {
+                    return;
+                }
+
+                $fields = [
+                    'p.nameFull' => 'p.nameFull',
+                    'address1926' => 'JSON_UNQUOTE(JSON_EXTRACT(p.infoByYear, \'$."1926".address\'))',
+                ];
+
+                $parameters = [];
+
+                $words = preg_split('/\,?\s+/', trim($values['value']));
+                $andParts = [];
+                foreach ($words as $i => $word) {
+                    if (empty($word)) {
+                        continue;
+                    }
+
+                    // expression parameters
+                    $paramName = sprintf('p_fulltext_%d', $i);
+                    $parameters[$paramName] = self::mysql_regex_escape($word);
+
+                    $orParts = [];
+                    foreach ($fields as $fieldName => $expr) {
+                        // expression that represents the condition
+                        $orParts[] = 'REGEXP(LOWER(' . $expr . '), LOWER(:' . $paramName . ')) = true';
+                    }
+
+                    $andParts[] = '(' . implode(' OR ', $orParts) . ')';
+                }
+
+                $expression = $filterQuery->getExpr()
+                    ->andX(join(' AND ', $andParts));
 
                 return $filterQuery->createCondition($expression, $parameters);
             }
